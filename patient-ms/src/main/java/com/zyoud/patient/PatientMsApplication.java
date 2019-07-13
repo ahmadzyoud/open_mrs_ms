de753f77289cbbed021c39676584d30e7f6e06d8
ahmad_zyoud <ahmadzyoud@gmail.com>
2019-07-13 16:11:07 +0300

    Patient MS
    First Draft

---
+++ patient-ms/src/main/java/com/zyoud/patient/PatientMsApplication.java
@@ -0,0 +1,13 @@
+package com.zyoud.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientMsApplication.class, args);
+	}
+
+}
